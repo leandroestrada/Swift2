445a87daf7845ab1ca26713e68f7159632479eda
leandroestrada <leandro.a.estrada@gmail.com>
2018-08-08 10:38:27 -0300

    Starting swift2 repo

---
+++ layoutTraining/layoutTraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  layoutTraining
+//
+//  Created by le on 08/08/2018.
+//  Copyright © 2018 LeandroEstrada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
